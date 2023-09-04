7a4b1b59624dcb78207acbdbbc8480437f3e1bf6
DevSharma06 <deven2806@gmail.com>
2023-09-04 20:54:35 +0530

    Added profile icon with dropdown in home page

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,10 +1,40 @@
 import { Link } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
 import { useLogout } from "../hooks/useLogout";
 import { useAuthContext } from "../hooks/useAuthContext";
+import { MdAccountCircle } from "react-icons/md";
+import userLogo from "../assets/images/user.png";
 
 const Navbar = () => {
   const { logout } = useLogout();
   const { user } = useAuthContext();
+  const [isDropdownVisible, setDropdownVisible] = useState(false);
+  const dropdownRef = useRef(null);
+
+  const toggleDropdown = () => {
+    setDropdownVisible(!isDropdownVisible);
+  };
+
+  const closeDropdown = () => {
+    setDropdownVisible(false);
+  };
+
+  useEffect(() => {
+    // Function to close the dropdown when a click occurs outside of it
+    function handleClickOutside(e) {
+      if (dropdownRef.current && !dropdownRef.current.contains(e.target)) {
+        setDropdownVisible(false);
+      }
+    }
+
+    // Add the event listener to the document
+    document.addEventListener("mousedown", handleClickOutside);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   const showAddProblem = () => {
     if (user) {
@@ -32,12 +62,31 @@ const Navbar = () => {
           <button>Logout</button>
         </div> */}
         <ul>
+          {showAddProblem()}
           {user && (
-            <li>
-              <Link onClick={handleLogout}>Logout</Link>
+            <li className="dropdown" ref={dropdownRef}>
+              <div className="account-icon">
+                <MdAccountCircle
+                  style={{ color: "#1aac83", fontSize: "70px" }}
+                  onClick={toggleDropdown}
+                />
+              </div>
+              {isDropdownVisible && (
+                <ul
+                  className={`dropdown-content ${
+                    isDropdownVisible ? "active" : ""
+                  }`}
+                >
+                  <li onClick={closeDropdown}>
+                    <Link>{user.email}</Link>
+                  </li>
+                  <li onClick={closeDropdown}>
+                    <Link onClick={handleLogout}>Logout</Link>
+                  </li>
+                </ul>
+              )}
             </li>
           )}
-          {showAddProblem()}
           {!user && (
             <li>
               <Link to="/register">Register</Link>
@@ -46,11 +95,6 @@ const Navbar = () => {
           {!user && (
             <li>
               <Link to="/login">Login</Link>
-            </li>
-          )}
-          {user && (
-            <li>
-              <Link>{user.email}</Link>
             </li>
           )}
         </ul>
